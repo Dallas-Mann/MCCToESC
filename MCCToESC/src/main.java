b82901a384befe1a291f942a17b7ed849e88b657
Dallas Mann <dallas.mann94@gmail.com>
2016-09-27 18:57:51 -0600

    Delete main.java

--- MCCToESC/src/main.java
+++
@@ -1,8 +0,0 @@
-
-public class main {
-	public static void Main(String[] args){
-		Nanotube nanotube = new Nanotube(Integer.parseInt(args[0]), Double.parseDouble(args[1]));
-		nanotube.printMCC();
-		nanotube.printESC();
-	}
-}
