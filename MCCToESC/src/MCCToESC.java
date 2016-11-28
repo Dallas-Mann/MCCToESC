326056dc1824639712b0e30ca22e686ccc3706ec
Dallas Mann @ Laptop <Dallas.mann94@gmail.com>
2016-11-28 15:47:21 -0700

    Added scattering resistance back in.

--- MCCToESC/src/MCCToESC.java
+++ MCCToESC/src/MCCToESC.java
@@ -5,8 +5,8 @@ public class MCCToESC{
 	public static void main(String[] args){
 		try{
 			int numberOfShells = Integer.parseInt(args[0]);
-			double diameterInnermostShell = Double.parseDouble(args[1]);
-			double distanceToGroundPlane = Double.parseDouble(args[2]);
+			double diameterInnermostShell = convert(args[1]);
+			double distanceToGroundPlane = convert(args[2]);
 			String outputFileName = args[3];
 			
 			Nanotube nanotube = new Nanotube(numberOfShells, diameterInnermostShell, distanceToGroundPlane);
@@ -23,5 +23,61 @@ public class MCCToESC{
 	public static void usage(){
 		System.out.println("Correct Usage: MCCToESC.java <number of shells> <diameter of innermost shell> "
 				+ "<distance to ground plane> <filename.txt>");
+	}
+	
+	private static double convert(String token){
+		double baseNum;
+		int indexOfModifier = token.length();
+		for(int i = 0; i < token.length(); i++){
+			char temp = token.charAt(i);
+			if(temp != 'E' && Character.isAlphabetic(temp)){
+				indexOfModifier = i;
+				break;
+			}
+		}
+		String modifier = token.substring(indexOfModifier);
+		String value = token.substring(0, indexOfModifier);
+		baseNum = Double.parseDouble(value);
+		//there should be a trailing modifier after the number
+		/*
+		F	E-15	femto
+		P	E-12	pico
+		N	E-9		nano
+		U	E-6		micro
+		M	E-3		milli
+		K	E+3		kilo
+		MEG E+6 	mega
+		G 	E+9 	giga
+		T 	E+12 	tera
+		 */
+		switch(modifier){
+			case "f":
+				return baseNum *= Math.pow(10, -15);
+			case "p":
+				return baseNum *= Math.pow(10, -12);
+			case "n":
+				return baseNum *= Math.pow(10, -9);
+			case "u":
+				return baseNum *= Math.pow(10, -6);
+			case "m":
+				return baseNum *= Math.pow(10, -3);
+			case "k":
+				return baseNum *= Math.pow(10, 3);
+			case "meg":
+				return baseNum *= Math.pow(10, 6);
+			case "g":
+				return baseNum *= Math.pow(10, 9);
+			case "t":
+				return baseNum *= Math.pow(10, 12);
+			default:
+				try{
+					if(token.chars().allMatch(Character::isDigit)){
+						baseNum = Double.parseDouble(token);
+					}
+				}
+				catch(Exception e){
+				}
+				return baseNum;	
+		}
 	}
 }
--- MCCToESC/src/Nanotube.java
+++ MCCToESC/src/Nanotube.java
@@ -10,14 +10,15 @@ public class Nanotube{
 	public static final double distanceBetweenShells = VANDER_WAALS_GAP;
 	
 	private List<Shell> shells;
+	private double diameterInnermostShell;	
+	protected double diameterOutermostShell;
+	protected double meanFreePath;
+	protected int numberOfShells;
+	protected int numberOfSections = 200;
 	
-	private int numberOfShells;
-	private double diameterInnermostShell;
-	private double distanceToGroundPlane;
-	
-	private double diameterOutermostShell;
-	private double meanFreePath;
 	private double groundCapacitance;
+	private double distanceToGroundPlane;
+
 	
 	public Nanotube(int numberOfShells, double diameterInnermostShell, double distanceToGroundPlane){
 		this.numberOfShells = numberOfShells;
@@ -43,7 +44,7 @@ public class Nanotube{
 		double diameterCurrentShell = diameterInnermostShell;
 		
 		for(int currentShell = 1; currentShell <= numberOfShells; currentShell++){
-			shells.add(new Shell(diameterCurrentShell, meanFreePath, currentShell, numberOfShells, distanceBetweenShells, diameterOutermostShell));
+			shells.add(new Shell(diameterCurrentShell, currentShell, this));
 			diameterCurrentShell += distanceBetweenShells;
 		}
 	}
@@ -57,70 +58,11 @@ public class Nanotube{
 	// this is used when converting from the MCC to the ESC model
 	public double sumQuantumCapacitance(int index){
 		Shell temp = shells.get(index);
-		if(temp.currentShell == temp.numberOfShells){
+		if(temp.currentShell == numberOfShells){
 			return 1.0/((1.0/temp.quantumCapacitance) + (1.0/temp.electrostaticCapacitance));
 		}
 		else
 			return 1.0/((1.0/temp.quantumCapacitance) + (1.0/temp.electrostaticCapacitance)) + sumQuantumCapacitance(index + 1);
-	}
-	
-	public void printESC(PrintWriter writer) throws FileNotFoundException{
-		double scatteringResistanceTotal = 0;
-		double contactQuantumResistanceTotal = 0;
-		double kineticInductanceTotal = 0;
-		double tempResistance = 0;
-		for(Shell s : shells){
-			scatteringResistanceTotal += s.scatteringResistance;
-			contactQuantumResistanceTotal += s.contactQuantumResistance;
-			kineticInductanceTotal += s.kineticInductance;
-		}
-		
-		double quantumCapacitanceTotal = sumQuantumCapacitance();
-		double electrostaticCapacitance = shells.get(numberOfShells - 1).electrostaticCapacitance;
-		
-		writer.println("ESC Model");
-		writer.println("Scattering Resistance: " + scatteringResistanceTotal);
-		writer.println("Contact Quantum Resistance: " + contactQuantumResistanceTotal);
-		writer.println("Kinetic Inductance: " + kineticInductanceTotal);
-		writer.println("Quantum Capacitance: " + quantumCapacitanceTotal);
-		writer.println("Electrostatic Capacitance: " + electrostaticCapacitance);
-		writer.println();
-		
-		PrintWriter escNetlist = new PrintWriter(new File("escNetlist.txt"));
-		int startNode = 3;
-		int nodeOne = 0;
-		int nodeTwo = 0;
-		int numCap = 0;
-		int numInd = 0;
-		
-		escNetlist.println("R0" + " " + 1 + " " + 2 + " " + contactQuantumResistanceTotal/2.0);
-		escNetlist.println("R1" + " " + 2 + " " + 3 + " " + scatteringResistanceTotal/2.0);
-		
-		for(int i = 0; i < 200; i++){
-			if(i == 0){
-				nodeOne = startNode;
-				nodeTwo = startNode + 1;
-			}
-			else{
-				nodeOne = 2 + 2*i;
-				nodeTwo = 4 + 2*i;
-			}
-			
-			escNetlist.println("L" + numInd + " " + nodeOne + " " + nodeTwo + " " + kineticInductanceTotal);
-						
-			escNetlist.println("C" + numCap + " " + nodeTwo + " " + (nodeTwo+1) + " " + contactQuantumResistanceTotal);
-			
-			escNetlist.println("C" + (numCap+1) + " " + (nodeTwo+1) + " " + 0 + " " + electrostaticCapacitance);
-			
-			numCap += 2;
-			numInd += 1;
-		}
-		
-		escNetlist.println("R2" + " " + nodeTwo + " " + (nodeTwo+2) + " " + contactQuantumResistanceTotal/2.0);
-		escNetlist.println("R3" + " " + (nodeTwo+2) + " " + (nodeTwo+3) + " " + scatteringResistanceTotal/2.0);
-		
-		escNetlist.println(".end");
-		escNetlist.close();
 	}
 	
 	public void printMCC(PrintWriter writer){
@@ -138,5 +80,77 @@ public class Nanotube{
 			writer.println("Shell Diameter: " + s.shellDiameter);
 			writer.println();
 		}
+	}
+	
+	public void printESC(PrintWriter writer) throws FileNotFoundException{
+		double imperfectContactResistance = 0;
+		double scatteringResistance = 0;
+		double contactQuantumResistance = 0;
+		double kineticInductance = 0;
+		
+		//sum resistances and inductance in parallel, because the shells are in parallel
+		for(Shell s : shells){
+			imperfectContactResistance += 1.0/s.imperfectContactResistance;
+			scatteringResistance += 1.0/s.scatteringResistance;
+			contactQuantumResistance += 1.0/s.contactQuantumResistance;
+			kineticInductance += 1.0/s.kineticInductance;
+		}
+		//final inversion, to sum in parallel
+		//example: 1/R = 1/r1 + 1/r2
+		imperfectContactResistance = 1.0/imperfectContactResistance;
+		contactQuantumResistance = 1.0/contactQuantumResistance;
+		
+		//outoput these values per micrometer
+		//need to divide by number of sections to get section parameters for the netlist
+		scatteringResistance = 1.0/scatteringResistance;
+		kineticInductance = 1.0/kineticInductance;
+		double quantumCapacitance = sumQuantumCapacitance();
+		double electrostaticCapacitance = shells.get(numberOfShells - 1).electrostaticCapacitance;
+		
+		writer.println("ESC Model");
+		writer.println("Scattering Resistance: " + scatteringResistance);
+		writer.println("Contact Quantum Resistance: " + contactQuantumResistance);
+		writer.println("Kinetic Inductance: " + kineticInductance);
+		writer.println("Quantum Capacitance: " + quantumCapacitance);
+		writer.println("Electrostatic Capacitance: " + electrostaticCapacitance);
+		writer.println();
+		
+		PrintWriter escNetlist = new PrintWriter(new File("escNetlist.txt"));
+		int nodeOne = 3;
+		
+		int numRes = 0;
+		int numInd = 0;
+		int numCap = 0;
+
+		
+		escNetlist.println("R0" + " " + 1 + " " + 2 + " " + contactQuantumResistance/2.0);
+		escNetlist.println("R1" + " " + 2 + " " + 3 + " " + imperfectContactResistance/2.0);
+		
+		for(int i = 0; i < 200; i++){
+			if(i == 0){
+				nodeOne = 3;
+			}
+			else{
+				nodeOne = (3 * i) + 2;
+			}
+			
+			escNetlist.println("R" + numRes + " " + nodeOne + " " + (nodeOne + 1) + " " + scatteringResistance);
+			
+			escNetlist.println("L" + numInd + " " + (nodeOne + 1) + " " + (nodeOne + 2) + " " + kineticInductance);
+						
+			escNetlist.println("C" + numCap + " " + (nodeOne + 2) + " " + (nodeOne + 3) + " " + quantumCapacitance);
+			
+			escNetlist.println("C" + (numCap+1) + " " + (nodeOne + 3) + " " + 0 + " " + electrostaticCapacitance);
+			
+			numCap += 2;
+			numInd += 1;
+			numRes += 1;
+		}
+		
+		escNetlist.println("R2" + " " + nodeOne + " " + (nodeOne+2) + " " + contactQuantumResistance/2.0);
+		escNetlist.println("R3" + " " + (nodeOne+2) + " " + (nodeOne+3) + " " + imperfectContactResistance/2.0);
+		
+		escNetlist.println(".end");
+		escNetlist.close();
 	}
 }
