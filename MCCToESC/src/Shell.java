a72d987df5d8fc43ce97536cec29b03d264593e1
Dallas Mann <Dallas.mann94@gmail.com>
2016-11-28 16:10:03 -0700

    Shell got removed from tracking.

--- MCCToESC/src/Shell.java
+++ MCCToESC/src/Shell.java
@@ -3,13 +3,13 @@ public class Shell{
 	public static final double PLANKS_CONSTANT = Math.pow(10, -34) * 6.62;
 	public static final double VANDER_WAALS_GAP = Math.pow(10, -9) * 0.34;
 	public static final double FERMI_VELOCITY = Math.pow(10, 5) * 8;
-	public static final double COULOMBS_CONSTANT = Math.pow(10, -19) * 1.6;
+	public static final double COULOMBS_CONSTANT = Math.pow(10, -19) * 1.602;
 	public static final double EPSILON = 8.85418782 * Math.pow(10, -12);
 	
 	// not currently used constant, but we think it might be used.
 	public static final double EPSILON_NAUGHT = 2;
-	
 	// instance variables
+	protected double imperfectContactResistance = 100000;
 	protected double contactQuantumResistance;
 	protected double scatteringResistance;
 	protected double kineticInductance;
@@ -20,20 +20,13 @@ public class Shell{
 	
 	// parameters passed in
 	protected double shellDiameter;
-	protected double meanFreePath;
 	protected int currentShell;
-	protected double numberOfShells;
-	protected double distanceBetweenShells;
-	protected double diameterOutermostShell;
+	protected Nanotube nanotube;
 	
-	public Shell(double shellDiameter, double meanFreePath, 
-			int currentShell, int numberOfShells, double distanceBetweenShells, double diameterOutermostShell){
+	public Shell(double shellDiameter, int currentShell, Nanotube nanotube){
 		this.shellDiameter = shellDiameter;
-		this.meanFreePath = meanFreePath;
 		this.currentShell = currentShell;
-		this.numberOfShells = numberOfShells;
-		this.distanceBetweenShells = distanceBetweenShells;
-		this.diameterOutermostShell = diameterOutermostShell;
+		this.nanotube = nanotube;
 		calcNumConductingChannels();
 		calcContactQuantumResistance();
 		calcScatteringResistance();
@@ -45,7 +38,7 @@ public class Shell{
 
 	private void calcNumConductingChannels(){
 		if(shellDiameter > 4.3 * Math.pow(10, -9)){
-			numConductingChannels = 0.116 * shellDiameter + 0.2;
+			numConductingChannels = 0.2 + (0.116 * shellDiameter * Math.pow(10, 9));
 		}
 		else
 			numConductingChannels = 2.0/3.0;
@@ -56,8 +49,8 @@ public class Shell{
 	}
 	
 	private void calcScatteringResistance(){
-		scatteringResistance = PLANKS_CONSTANT/
-				(2 * COULOMBS_CONSTANT * COULOMBS_CONSTANT * numConductingChannels * meanFreePath);
+		scatteringResistance = PLANKS_CONSTANT*Math.pow(10, -6)/
+				(2 * COULOMBS_CONSTANT * COULOMBS_CONSTANT * numConductingChannels * nanotube.meanFreePath);
 	}
 	
 	private void calcKineticInductance(){
@@ -72,17 +65,18 @@ public class Shell{
 	}
 	
 	private void calcElectrostaticCapacitance(){
-		if(currentShell < numberOfShells){
+		if(currentShell < nanotube.numberOfShells){
 			electrostaticCapacitance = 2 * Math.PI * EPSILON /
-					Math.log((shellDiameter + distanceBetweenShells)/shellDiameter);
+					Math.log((shellDiameter + Nanotube.distanceBetweenShells)/shellDiameter);
 		}
 		else
-			electrostaticCapacitance = Math.PI * EPSILON / Math.log((2 * 22 * Math.pow(10, -9)/diameterOutermostShell)-1);
+			electrostaticCapacitance = Math.PI * EPSILON / Math.log((2 * 50 * Math.pow(10, -9)/nanotube.diameterOutermostShell)-1);
 	}
 	
 	private void calcInnerShellConductance(){
-		if(currentShell != numberOfShells){
-			innerShellConductance = Math.pow(10, 4) * Math.PI * shellDiameter / VANDER_WAALS_GAP;
+		if(currentShell != nanotube.numberOfShells){
+			innerShellConductance = Math.pow(10, -2)
+					* Math.PI * shellDiameter / VANDER_WAALS_GAP;
 		}
 		else
 			innerShellConductance = 0;
